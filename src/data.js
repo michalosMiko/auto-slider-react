31197a72dcdf36745de6df675ad04e73940de7f2
MichalMamula <michalosm@centrum.cz>
2023-01-09 20:12:59 +0100

    autoreactslider

--- src/data.js
+++ src/data.js
@@ -1,43 +1,43 @@
-import image1 from "./images/movie1.png"
-import image2 from "./images/movie2.png"
-import image3 from "./images/movie3.png"
-import image4 from "./images/movie4.png"
+import image1 from "./images/Himawari 1.png"
+import image2 from "./images/Himawari 2.png"
+import image3 from "./images/Himawari 3.png"
+import image4 from "./images/Himawari 4.png"
 
 const allMovies = [
     {
         id: 1,
         image: image1,
-        title: "Enola Holmesová 2",
-        age: "+13",
-        tags: "Vtipný * Napínavý * Rodina",
-        description: "Nulla pulvinar eleifend sem. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta",
+        title: "Himawari",
+        age: "March 2014",
+        tags: "Satellite * Sunfower *  meteorological ",
+        description: "The Himawari (ひまわり, “sunflower”) geostationary satellites, operated by the Japan Meteorological Agency (JMA), support weather forecasting, tropical cyclone tracking, and meteorology research. Most meteorological agencies in East Asia, Southeast Asia, Australia and New Zealand use the satellites for their own weather monitoring and forecasting operations.",
         category: "komedie"
     },
     {
         id: 2,
         image: image2,
-        title: "Enola Holmesová",
-        age: "+13",
-        tags: "Vtipný * Napínavý * Rodina",
-        description: "Etiam egestas wisi a erat. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Integer imperdiet lectus quis justo. Vivamus luctus egestas leo",
+        title: "Earth",
+        age: " 4.5 billion years ago ",
+        tags: "Ellipsoid with a circumference of about 40,000 km",
+        description: "Earth is the third planet from the Sun and the only place in the universe known to harbor life. While large volumes of water can be found throughout the Solar System, only Earth sustains liquid surface water.",
         category: "komedie"
     },
     {
         id: 3,
         image: image3,
-        title: "Rocknrolla",
-        age: "+16",
-        tags: "Gangster * Napínavý * Násilí",
-        description: "Nulla pulvinar eleifend sem. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta",
+        title: "Night on Earth",
+        age: "night time",
+        tags: "The opposite of night is day",
+        description: "Night  is the period of ambient darkness from sunset to sunrise during each 24-hour day, when the Sun is below the horizon. The exact time when night begins and ends depends on the location and varies throughout the year, based on factors such as season and latitude.",
         category: "romantický"
     },
     {
         id: 4,
         image: image4,
-        title: "Patriot",
-        age: "+16",
-        tags: "Gangster * Napínavý * Násilí",
-        description: "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Nulla quis diam. Nulla pulvinar eleifend sem. Maecenas lorem. Fusce suscipit libero eget.",
+        title: "React on Earth",
+        age: "",
+        tags: "JSX * Componet * App",
+        description: "React A JavaScript library for building user interfaces",
         category: "romantický"
     },
 ]
